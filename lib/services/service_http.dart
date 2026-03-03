17cd8d6e65a71d9af7b9e6bc64a00c45f5fbc031
mddhafa <mdmuhammaddhafa1234@gmail.com>
2026-03-03 22:22:15 +0700

    service http

--- lib/services/service_http.dart
+++ lib/services/service_http.dart
@@ -4,7 +4,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
 class ServiceHttp {
-  final String baseUrl = 'http://10.0.2.2:3000/api/';
+  final String baseUrl = 'http://192.168.18.15:3000/api/';
   final Storage = FlutterSecureStorage();
 
   //post
